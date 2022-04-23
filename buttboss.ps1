beef9616747a6dae2d238c9ef573877c6b29ccdb
mellonaut <rleeazw@outlook.com>
2022-04-23 15:41:37 -0400

    Update buttboss.ps1

--- buttboss.ps1
+++ buttboss.ps1
@@ -1,22 +1,16 @@
-# Enumerate targets
-
 # Set Desktop Path, switch to OneDrive path format if OneDrive folder exists
 $desktop = "$home\Desktop"
 if((Test-Path $env:OneDrive) -Or (Test-Path $env:OneDriveCommercial)) {
     $desktop = "$env:OneDrive\Desktop"
 }
 
-# 
-# $env:OneDriveConsumer
-
-
 # Copy Desktop Contents to Goofin for safe keeping
 Copy-Item -Path $desktop -Destination "C:\Goofin\cuts" -Recurse -Force
 
-$targets = (gci $home\OneDrive\Desktop\ -Recurse)
-# Grab number of items we need butts for 
-$buttnum = (echo $targets | Get-Member)
-$buttnum = $buttnum.Count
+# enumerate targets for replacement & grab number of items we need butts for 
+$butts = (gci $home\OneDrive\Desktop\ -Recurse)
+$buttlist = (echo $butts | Get-Member)
+$buttnum = $buttlist.Count
 
 function Search-Flickr ([string]$tags, [int]$count = $buttnum) 
 {
@@ -38,6 +32,8 @@ return $url
 $topic = "juicy butts "+(Get-Random) 
 $url = Search-Flickr $topic
 
+
+
     for each target in targets
         Where extension = any image extension
             Copy each item with original filename to Goofin/cuts        
@@ -46,15 +42,14 @@ $url = Search-Flickr $topic
             Copy each item with original filename to Goofin/cuts
             leave icon the same, just redirect link to open a google search for random butt search
 
+
+
 # Hit 'em with it
 cd Desktop  notepad buttboss.txt writefile "Now who's boss."  
-
 
 # Politeness Module
 # Reset the photots and icons after X minutes
 
-
 # Persistence Module
-
 # Schduled task for the script
 # Could we set it to choose a number between 7-20 = x and create a that runs every x days?
